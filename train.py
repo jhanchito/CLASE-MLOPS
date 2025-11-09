e1f7a36cfa1447f6b7461ebf6c2aa3a7f0724539
jhampier <jhampiertapia@gmail.com>
2025-11-08 19:02:36 -0500

    Envio 5

--- train.py
+++ train.py
@@ -1,5 +1,11 @@
 import pandas as pd
 import numpy as np
+import re
+import pickle
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+from pathlib import Path
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, LSTM, Embedding
 from tensorflow.keras.preprocessing.text import Tokenizer
@@ -7,11 +13,13 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from tensorflow.keras.utils import to_categorical
-from sklearn.metrics import accuracy_score, classification_report, mean_absolute_error, mean_squared_error, confusion_matrix
-from pathlib import Path
-
-import re
-import pickle
+from sklearn.metrics import (
+    accuracy_score, 
+    classification_report, 
+    mean_absolute_error, 
+    mean_squared_error, 
+    confusion_matrix
+)
 
 DATA_PATH = Path(__file__).resolve().parent / "data" / "base_encuestados_v2.csv"
 df = pd.read_csv(DATA_PATH).head(1000)     
